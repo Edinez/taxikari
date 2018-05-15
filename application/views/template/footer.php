4966e075b0b8310a841b7e50629fbaccfa24e0ba
Eduard Gabel <edi.gieci@gmail.com>
2018-05-15 14:48:39 +0200

    pagination spravena

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -22,6 +22,98 @@
     $('.clockpicker').clockpicker();
 </script>
 
+
+<style >
+    li.farbacisiel{
+        margin-right:10px;
+        color:blue;
+    }
+</style>
+
+<script>
+    var table ='#mytable';
+    $('#maxRows').on('change',function () {
+        $('.pagination').html('');
+        var trnum=0;
+        var maxRows = parseInt($(this).val());
+        var totalRows = $(table+' tbody tr').length;
+        $(table+' tr:gt(0)').each(function () {
+            trnum++;
+            if(trnum>maxRows){
+                $(this).hide()
+            }
+            if(trnum <=maxRows){
+                $(this).show()
+            }
+        });
+        if(totalRows > maxRows){
+            var pagenum = Math.ceil(totalRows/maxRows);
+            for(var i=1;i<=pagenum;){
+                $('.pagination').append('<li class="farbacisiel"  data-page="'+i+'">\<span>'+ i++ +'<span class="sr-only">(current)</span></span>\</li>').show()
+            }
+        }
+        $('.pagination li:first-child').addClass('active');
+        $('.pagination li').on('click',function () {
+            var pageNum =$(this).attr('data-page');
+            var trIndex = 0
+            $('.pagination li').removeClass('active');
+            $(this).addClass('active');
+            $(table+' tr:gt(0)').each(function () {
+                trIndex++
+                if(trIndex > (maxRows*pageNum) || trIndex <=((maxRows*pageNum)-maxRows)) {
+                    $(this).hide()
+                } else {
+                    $(this).show()
+                }
+            })
+        })
+    })
+
+</script>
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
+
+<script>
+    var ctx = document.getElementById("myChart");
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+</script>
+
 <script>
     $(document).ready(function(){
         var date_input=$('input[name="Datum_Od"]');
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -3,20 +3,31 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 
 
-<style >h3 {
+<style>h3 {
         margin-top: 80px;
     } </style>
 
 <div class="container" id="objednavka">
-    <h3> Tabulka objednávok  </h3>
-
-    <a class=" btn btn-success" href="<?php echo site_url('home/createobjednavka');?>">
+    <h3> Tabulka objednávok </h3>
+    <div class="form-group" >
+        <select name="state" id="maxRows" class="form-control" style="width:150px; height:40px" >
+            <option value="5000">Všetky</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="5">5</option>
+            <option value="7">7</option>
+            <option value="10">10</option>
+            <option value="20">20</option>
+        </select>
+    </div>
+    <a class=" btn btn-success" href="<?php echo site_url('home/createobjednavka'); ?>">
         Vytvor objednávku
     </a>
-    <?php if($msg =$this->session->flashdata('msg_objednavka')): ?>
+    <?php if ($msg = $this->session->flashdata('msg_objednavka')): ?>
         <?php echo $msg; ?>
     <?php endif; ?>
-    <table class="table" >
+    <table id="mytable" class="table table-bordered table-striped">
         <thead>
         <tr>
             <th scope="col">Začiatok jazdy</th>
@@ -27,7 +38,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             <th scope="col">Čas</th>
             <th scope="col">Číslo smeny</th>
             <th scope="col">Meno zákazníka</th>
-            <th scope="col"></th>
+            <th scope="col">Možnosti</th>
         </tr>
         </thead>
         <tbody>
@@ -35,12 +46,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             <tr>
                 <td><?php echo $objednavka_data['zaciatok']; ?></td>
                 <td><?php echo $objednavka_data['ciel']; ?></td>
-                <td><?php echo $objednavka_data['vzdialenost']." km"; ?></td>
-                <td><?php echo $objednavka_data['platit']." €"; ?></td>
+                <td><?php echo $objednavka_data['vzdialenost'] . " km"; ?></td>
+                <td><?php echo $objednavka_data['platit'] . " €"; ?></td>
                 <td><?php echo $objednavka_data['datum']; ?></td>
                 <td><?php echo $objednavka_data['cas']; ?></td>
                 <td><?php echo $objednavka_data['smeny']; ?></td>
-                <td><?php echo $objednavka_data['meno']." ".$objednavka_data['priezvisko']; ?></td>
+                <td><?php echo $objednavka_data['meno'] . " " . $objednavka_data['priezvisko']; ?></td>
                 <td>
                     <?php echo anchor("home/view_objednavka/{$objednavka_data['idObjednavka']}", 'Podrobnosti', ['class' => 'btn btn-primary']); ?>
                 </td>
@@ -48,19 +59,25 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <?php endforeach; ?>
         </tbody>
     </table>
+    <div class="pagination-container">
+        <nav>
+            <ul class="pagination"></ul>
+        </nav>
+    </div>
 </div>
 
+<!-- <canvas id="myChart" width="50" height="50"></canvas>  -->
 
 <div class="container" id="smeny">
     <h3> Tabulka pracovnych smien </h3>
 
-    <a class=" btn btn-success" href="<?php echo site_url('home/createsmena');?>">
+    <a class=" btn btn-success" href="<?php echo site_url('home/createsmena'); ?>">
         Pridaj smenu
     </a>
-    <?php if($msg =$this->session->flashdata('msg_smena')): ?>
+    <?php if ($msg = $this->session->flashdata('msg_smena')): ?>
         <?php echo $msg; ?>
     <?php endif; ?>
-    <table class="table" >
+    <table  class="table table-bordered table-striped">
         <thead>
         <tr>
             <th scope="col">Dátum od</th>
@@ -69,24 +86,24 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             <th scope="col">Čas do</th>
             <th scope="col">Vodič</th>
             <th scope="col">Vozidlo na smene</th>
-            <th scope="col"></th>
+            <th scope="col">Možnosti</th>
         </tr>
         </thead>
         <tbody>
-            <?php foreach ($smena2 as $smena_data): ?>
-                <tr>
-                    <td><?php echo $smena_data['Datum_Od']; ?></td>
-                    <td><?php echo $smena_data['Datum_Do']; ?></td>
-                    <td><?php echo $smena_data['Cas_Od']; ?></td>
-                    <td><?php echo $smena_data['Cas_Do']; ?></td>
-                    <td><?php echo $smena_data['vodicmeno']." ".$smena_data['vodicpriezvisko']; ?></td>
-                    <td><?php echo $smena_data['znacka']." ".$smena_data['model']; ?></td>
-                    <td>
-                        <?php echo anchor("home/view_smena/{$smena_data['idSmeny']}", 'Podrobnosti', ['class' => 'btn btn-primary']); ?>
-                        <?php echo anchor("home/delete_smena/{$smena_data['idSmeny']}", 'Vymazať', ['class' => 'btn btn-danger']); ?>
-                    </td>
-                </tr>
-            <?php endforeach; ?>
+        <?php foreach ($smena2 as $smena_data): ?>
+            <tr>
+                <td><?php echo $smena_data['Datum_Od']; ?></td>
+                <td><?php echo $smena_data['Datum_Do']; ?></td>
+                <td><?php echo $smena_data['Cas_Od']; ?></td>
+                <td><?php echo $smena_data['Cas_Do']; ?></td>
+                <td><?php echo $smena_data['vodicmeno'] . " " . $smena_data['vodicpriezvisko']; ?></td>
+                <td><?php echo $smena_data['znacka'] . " " . $smena_data['model']; ?></td>
+                <td>
+                    <?php echo anchor("home/view_smena/{$smena_data['idSmeny']}", 'Podrobnosti', ['class' => 'btn btn-primary']); ?>
+                    <?php echo anchor("home/delete_smena/{$smena_data['idSmeny']}", 'Vymazať', ['class' => 'btn btn-danger']); ?>
+                </td>
+            </tr>
+        <?php endforeach; ?>
         </tbody>
     </table>
 </div>
@@ -95,13 +112,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <div class="container" id="zakaznici">
     <h3> Tabulka zakaznici </h3>
 
-    <a class=" btn btn-success" href="<?php echo site_url('home/create_zakaznik');?>">
+    <a class=" btn btn-success" href="<?php echo site_url('home/create_zakaznik'); ?>">
         Pridaj zákazníka
     </a>
-    <?php if($msg =$this->session->flashdata('msg_zakaznici')): ?>
+    <?php if ($msg = $this->session->flashdata('msg_zakaznici')): ?>
         <?php echo $msg; ?>
     <?php endif; ?>
-    <table class="table" >
+    <table class="table table-bordered table-striped">
         <thead>
         <tr>
             <th scope="col">Meno</th>
@@ -109,7 +126,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             <th scope="col">Tel. kontakt</th>
             <th scope="col">Email</th>
             <th scope="col">Mesto</th>
-            <th scope="col"></th>
+            <th scope="col">Možnosti</th>
         </tr>
         </thead>
         <tbody>
@@ -145,20 +162,20 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <div class="container" id="vodici">
     <h3> Tabulka vodiči </h3>
 
-    <a class=" btn btn-success" href="<?php echo site_url('home/createvodic');?>">
+    <a class=" btn btn-success" href="<?php echo site_url('home/createvodic'); ?>">
         Pridaj vodiča
     </a>
-    <?php if($msg =$this->session->flashdata('msg_vodici')): ?>
+    <?php if ($msg = $this->session->flashdata('msg_vodici')): ?>
         <?php echo $msg; ?>
     <?php endif; ?>
-    <table class="table" >
+    <table class="table table-bordered table-striped">
         <thead>
         <tr>
             <th scope="col">Meno</th>
             <th scope="col">Priezvisko</th>
             <th scope="col">Tel. kontakt</th>
             <th scope="col">Vek</th>
-            <th scope="col"></th>
+            <th scope="col">Možnosti</th>
         </tr>
         </thead>
         <tbody>
@@ -168,7 +185,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                     <td><?php echo $vodici_data->Meno; ?></td>
                     <td><?php echo $vodici_data->Priezvisko; ?></td>
                     <td><?php echo $vodici_data->Tel_kontakt; ?></td>
-                    <td><?php echo $vodici_data->Vek." rokov"; ?></td>
+                    <td><?php echo $vodici_data->Vek . " rokov"; ?></td>
 
                     <td>
                         <?php echo anchor("home/view_vodici/{$vodici_data->idVodic}", 'Podrobnosti', ['class' => 'btn btn-primary']); ?>
@@ -191,19 +208,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <div class="container" id="vozidlo">
     <h3> Tabulka vozidlo </h3>
 
-    <a class=" btn btn-success" href="<?php echo site_url('home/createvozidlo');?>">
+    <a class=" btn btn-success" href="<?php echo site_url('home/createvozidlo'); ?>">
         Pridaj vozidlo
     </a>
-    <?php if($msg =$this->session->flashdata('msg_vozidlo')): ?>
+    <?php if ($msg = $this->session->flashdata('msg_vozidlo')): ?>
         <?php echo $msg; ?>
     <?php endif; ?>
-    <table class="table" >
+    <table class="table table-bordered table-striped">
         <thead>
         <tr>
             <th scope="col">Značka</th>
             <th scope="col">Model</th>
             <th scope="col">Rok</th>
-            <th scope="col"></th>
+            <th scope="col">Možnosti</th>
         </tr>
         </thead>
         <tbody>
